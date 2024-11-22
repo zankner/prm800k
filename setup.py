4b78e08c71fb2c1b2f7cb57bfbb9bf0832a76e31
Zack Ankner <zackankner@gmail.com>
2024-11-22 21:23:47 +0000

    setup

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
-import numpy as np
 
 setup(
     name='prm800k',
     packages=find_packages(),
     version='0.0.1',
+    install_requires=[
+        "pylatexenc",
+        "numpy"
+    ]
 )
